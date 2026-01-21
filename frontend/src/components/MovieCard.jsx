57b62a2d197c72c4c28ddd02e243b7a489e69fac
Shubham Makhija <shubhammakhija19@gmail.com>
2026-01-21 15:24:42 +0530

    fixed favourites

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,9 +1,18 @@
+import { useMovieContext } from "../contexts/MovieContext";
+
 function MovieCard({movie}){
+    const { addToFavorites, removeFromFavorites, isFavorite } = useMovieContext();
+    
     function OnFavourite(){
-        alert("clicked")
+        if(isFavorite(movie.id)){
+            removeFromFavorites(movie.id)
+        } else {
+            addToFavorites(movie)
+        }
     }
 
     const posterUrl = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;
+    const favorite = isFavorite(movie.id);
 
     return(
         <>
@@ -11,7 +20,7 @@ function MovieCard({movie}){
             <div className="movie-poster">
                 <img src={posterUrl} alt={movie.title} />
                 <div className="movie-overlay">
-                    <button className="favourite-btn" onClick={OnFavourite}>♥</button>
+                    <button className="favourite-btn" onClick={OnFavourite} style={{color: favorite ? "red" : "white"}}>♥</button>
                 </div>
             </div>
             <div className="movie-info">
